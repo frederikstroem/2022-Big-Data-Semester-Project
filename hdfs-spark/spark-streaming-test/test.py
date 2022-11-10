ac3485fbe6a9ae8d359829539d96547905a9075c
CoffeeOverload <ansor18@student.sdu.dk>
2022-11-10 13:13:18 +0100

    update to split data in hist/live

--- hdfs-spark/spark-streaming-test/test.py
+++ hdfs-spark/spark-streaming-test/test.py
@@ -22,12 +22,12 @@ def to_json_udf(value):
     return "{ \"type\": \"repo\", " + temp + " }"
 
 # Create SparkSession
-spark = SparkSession.builder.appName('initialTest') \
-    .config('spark.master','spark://spark-master:17077') \
-    .config('spark.executor.cores', 1) \
-    .config('spark.cores.max', 2) \
-    .config('spark.executor.memory', '1g') \
-    .config('spark.sql.streaming.checkpointLocation','hdfs://namenode:9000/stream-checkpoint/') \
+spark = SparkSession.builder.appName("initialTest") \
+    .config("spark.master","spark://spark-master:17077") \
+    .config("spark.executor.cores", 1) \
+    .config("spark.cores.max", 2) \
+    .config("spark.executor.memory", "2g") \
+    .config("spark.sql.streaming.checkpointLocation","hdfs://namenode:9000/stream-checkpoint/") \
     .getOrCreate()
 
 # Read from Kafka topic
@@ -40,26 +40,57 @@ df = spark \
     .load()
 
 # Transform DF
-cols = ["id", "name", "size", "description", "language", "watchers_count", "forks_count", "open_issues_count"]
+cols = ["id", "name", "description", "size", "watchers_count", "forks_count", "open_issues_count", "pushed_at", "updated_at"]
 
 df1 = df.selectExpr("CAST(value AS STRING)")
 df2 = df1.select(explode_outer(from_json(df1.value,MapType(StringType(),StringType()))))
 df2 = df2.select(df2.key.alias("key1"), df2.value.alias("value1"), explode_outer(from_json(df2.value,MapType(StringType(),StringType()))))
 df2 = df2.filter(df2.key1 == "payload")
 df2 = df2.select(df2.value1.alias("id"), explode_outer(from_json(df2.value,MapType(StringType(),StringType()))))
-df2 = df2.filter(df2.key.isin(cols))
-df2 = df2.withColumn("tmp", to_string_udf(df2.key, df2.value))
 
-result = df2.groupBy("id").agg(collect_set("tmp").alias("res"))
-result = result.withColumn("value", to_json_udf(result.res))
 
-# Write to Kafka topic
-result.select("value")\
+# Get date field
+date = df2.filter(df2.key == "pushed_at")
+
+# Filter based on date DF to get historical and live data
+dfh = date.filter("value < date'2013-07-30'")
+dfl = date.filter("value >= date'2013-07-30'")
+
+# Can"t use joins as they require outputMode("append"), and aggregations require complete.
+# Therefore data must be flattened again.
+
+# Flatten, retrieve columns, and group to json string
+dfh = dfh.select(dfh.id, explode_outer(from_json(dfh.id,MapType(StringType(),StringType()))))
+dfh = dfh.select(dfh.id, explode_outer(from_json(dfh.value,MapType(StringType(),StringType()))))
+dfh = dfh.filter(dfh.key.isin(cols))
+dfh = dfh.withColumn("tmp", to_string_udf(dfh.key, dfh.value))
+dfh = dfh.groupBy("id").agg(collect_set("tmp").alias("res"))
+dfh = dfh.withColumn("value", to_json_udf(dfh.res))
+
+# Flatten, retrieve columns, and group to json string
+dfl = dfl.select(dfl.id, explode_outer(from_json(dfl.id,MapType(StringType(),StringType()))))
+dfl = dfl.select(dfl.id, explode_outer(from_json(dfl.value,MapType(StringType(),StringType()))))
+dfl = dfl.filter(dfl.key.isin(cols))
+dfl = dfl.withColumn("tmp", to_string_udf(dfl.key, dfl.value))
+dfl = dfl.groupBy("id").agg(collect_set("tmp").alias("res"))
+dfl = dfl.withColumn("value", to_json_udf(dfl.res))
+
+# Write historical to Kafka topic
+dfh.select("value")\
     .writeStream\
-    .format('kafka')\
+    .format("kafka")\
     .option("kafka.bootstrap.servers", "kafka-1:19092,kafka-2:29092,kafka-3:39092") \
-    .option("topic", "msr14.repos.processed") \
+    .option("topic", "msr14.repos.historical") \
     .outputMode("complete") \
     .start() \
-    .awaitTermination()
+
+dfl.select("value")\
+    .writeStream\
+    .format("kafka")\
+    .option("kafka.bootstrap.servers", "kafka-1:19092,kafka-2:29092,kafka-3:39092") \
+    .option("topic", "msr14.repos.live") \
+    .outputMode("complete") \
+    .start() \
+
+spark.streams.awaitAnyTermination()
 
