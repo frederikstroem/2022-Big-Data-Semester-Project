a1ce8ad93df3ca2f4e84453eec7cc5582738103f
CoffeeOverload <ansor18@student.sdu.dk>
2022-11-20 15:53:40 +0100

    update to extract answers as aggregated stats from commits

--- hdfs-spark/spark-streaming/commits/app.py
+++ hdfs-spark/spark-streaming/commits/app.py
@@ -1,12 +1,10 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode_outer, from_json, lit, max, struct, to_json, udf
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import col, collect_list, desc, explode_outer, from_json, lit, max, rank, row_number, struct, sum, to_json, udf
 from pyspark.sql.types import MapType, StringType
 from datetime import date
 import schemas
 
-def get_html_url(value):
-    index = rfind("/commit"/)
-    return value[:index]
+global ANSWER_TOPIC
 
 def create_spark_session(app_name):
     spark = SparkSession.builder.appName(app_name) \
@@ -44,6 +42,54 @@ def write_to_kafka_topic(df, topic, mode):
         .outputMode(mode) \
         .start() \
 
+def batch_write_to_kafka_topic(df, foreach_batch_function):
+    df.writeStream \
+        .outputMode("append") \
+        .foreachBatch(foreach_batch_function) \
+        .start()
+
+def write_micro_batch_to_kafka_topic(df):
+    df.select(to_json(struct([df[x] for x in df.columns])).alias("value")).select("value") \
+        .write \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "kafka-1:19092,kafka-2:29092,kafka-3:39092") \
+        .option("topic", ANSWER_TOPIC) \
+        .mode("append") \
+        .save()
+
+def sum_stats(df, epoch_id):
+    # Total
+    df2 = partition_df(df, "total")
+    write_micro_batch_to_kafka_topic(df2)
+
+    # Additions
+    df2 = partition_df(df, "additions")
+    write_micro_batch_to_kafka_topic(df2)
+
+    # Deletions
+    df2 = partition_df(df, "deletions")
+    write_micro_batch_to_kafka_topic(df2)
+
+def partition_df(df, field):
+    field2 = "sum({})".format(field)
+    df2 = df.groupBy(df.category, df.repo) \
+            .sum()
+    w = Window.partitionBy("category").orderBy(desc(field2))
+    df2 = df2.withColumn("placement", rank().over(w))
+    df2 = df2.filter( \
+        ((df2.placement <= 3) & (df2.category == "all time")) | \
+            ((df2.placement == 1) & (df2.category == "latest")))   
+    df2 = df2.select(
+        df2.category, 
+        df2.placement, 
+        df2.repo, 
+        col(field2).alias("sum")
+    ) \
+        .withColumn("type", lit(field))
+    df2 = df2.groupBy(df2.category, df2.type) \
+             .agg(collect_list(struct("placement", "repo", "sum")).alias("placements"))      
+    return df2
+
 @udf
 def get_date_category_udf(value):
     if value is not None:
@@ -53,15 +99,15 @@ def get_date_category_udf(value):
     return "all time"
 
 @udf
-def get_html_url(value):
+def get_repo_udf(value):
     # Discard everything after commit in html_url
     index = value.rfind("/commit/")
-    return value[:index]
+    return value[19:index]
 
 
 if __name__ == '__main__':
-    APP_NAME = "repos-job"
-    TOPIC = "msr14.repos_limit100k"
+    APP_NAME = "commits-job"
+    TOPIC = "msr14.commits_limit100k"
     HIST_TOPIC = TOPIC + ".historical"
     ANSWER_TOPIC = "answers"
     SCHEMA = schemas.commits_schema
@@ -72,31 +118,17 @@ if __name__ == '__main__':
 
     # Query data.
     df = df.select(
-        df.fullDocument.date.alias("date"),
+        df.fullDocument.commit.committer.date.alias("commit_date"),
         df.fullDocument.html_url.alias("html_url"),
         df.fullDocument.stats.total.alias("total"),
         df.fullDocument.stats.additions.alias("additions"),
         df.fullDocument.stats.deletions.alias("deletions")
-    )
+    ) \
+        .withColumn("repo", get_repo_udf(df.html_url))
 
-#df2.select("fullDocument.html_url", "fullDocument.commit.comitter.date", "fullDocument.stats.additions", "fullDocument.stats.deletions").show(1, False)
-#df2.select("fullDocument.*").show()
-#df3 = df2.select("fullDocument.html_url", "fullDocument.commit.committer.date", "fullDocument.stats.additions", "fullDocument.stats.deletions")
-#df4 = df3.withColumn("formatted_url", get_html_url(df3.html_url))
-
-    df2 = df1.withColumn(formatted_url, get_html_url(df3.html_url))
-
-    # df2 = df.withColumn("tmp", lit("A")) \
-    #         .withColumn("category", get_date_category_udf(df.pushed_at))
-    # df2 = df2.groupBy(df2.tmp, df2.category).agg(
-    #         max(struct("watchers_count", "full_name", "category")).alias("watchers"),
-    #         max(struct("forks_count", "full_name", "category")).alias("forks"),
-    #         max(struct("open_issues_count", "full_name", "category")).alias("open_issues"),
-    #         max(struct("size", "full_name", "category")).alias("size")
-    #     ).drop("tmp", "category")
-
+    df2 = df.withColumn("category", get_date_category_udf(df.commit_date))
     write_to_kafka_topic(df, HIST_TOPIC, "append")
-    write_to_kafka_topic(df2, ANSWER_TOPIC, "complete")
+    batch_write_to_kafka_topic(df2, sum_stats)
 
     spark.streams.awaitAnyTermination()
     spark.stop()
