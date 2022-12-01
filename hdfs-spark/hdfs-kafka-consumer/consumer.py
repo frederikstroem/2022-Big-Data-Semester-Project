bb38b901811c9011ecc6272cc84eef43e084f728
Marcus Adam Sørensen <marcs19@student.sdu.dk>
2022-12-01 21:53:18 +0100

    BDSP-1: WIP work for parquet with pyarrow

--- hdfs-spark/hdfs-kafka-consumer/consumer.py
+++ hdfs-spark/hdfs-kafka-consumer/consumer.py
@@ -1,8 +1,11 @@
 from hdfs import InsecureClient
 from kafka import KafkaConsumer
+import pandas as pd
+from pyarrow import fs
 
 from datetime import datetime
 import time
+import json
 
 
 def printer(str):
@@ -14,14 +17,17 @@ def printer(str):
 printer("Kafka consumer init")
 
 client = InsecureClient("http://namenode:9870", user="root")
+hdfs = fs.FileSystem.from_uri("hdfs://namenode:9870")
 consumer = KafkaConsumer(
     "temp",
     bootstrap_servers=["kafka-1:19092", "kafka-2:29092", "kafka-3:39092"],
     auto_offset_reset="earliest",
     enable_auto_commit=True,
     group_id="group1",
-    value_deserializer=lambda x: x.decode("utf-8"),
+    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
 )
+
+printer(hdfs)
 
 for message in consumer:
     start = time.time()
@@ -33,8 +39,12 @@ for message in consumer:
     if value == "EOF":
         break
 
-    with client.write("/test.txt", encoding="utf-8", overwrite=True) as writer:
-        writer.write(value)
+    df = pd.json_normalize(value)
+
+    printer(df)
+
+    # with client.write("/test.txt", encoding="utf-8", overwrite=True) as writer:
+    #    writer.write(value)
 
     duration = time.time() - start
     printer(f"Processed message in {round(duration, 2)} seconds.")
